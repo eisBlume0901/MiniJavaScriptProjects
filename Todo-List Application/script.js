6a4cf2688f5d5222a88d511e9b0e62d2695f2729
Mary Kate F. Anecito <marykate09@protonmail.com>
2024-02-29 21:35:58 +0800

    Refactor addTask function for flexibility
    Modified the addTask function to accept a 'timeOfDay' parameter. This change allows tasks to be added to specific times of the day (morning, afternoon, evening) based on the input argument, enhancing the flexibility and usability of the task management feature.

--- Todo-List Application/script.js
+++ Todo-List Application/script.js
@@ -3,52 +3,43 @@ const taskInput = document.getElementById('task-input');
 const morningButton = document.getElementById('morning-button');
 const afternoonButton = document.getElementById('afternoon-button');
 const eveningButton = document.getElementById('evening-button');
-const taskList = document.getElementById('task-list');
 function sanitizeStringInput(str) {
     const regex = /[|&;$%@"<>()+,]/g;
     return str.replace(regex, '');
 }
 
 function isInputEmpty(str) {
-    if (str !== "") {
-        return false;
-    }
-    return true;
+    return str === "";
 }
+
 function addTask(timeOfDay) {
-    if (isInputEmpty(taskInput['value']))
-    {
+    if (isInputEmpty(taskInput['value'])) {
         alert("Please enter a task.")
     }
     else
     {
-        const task = sanitizeStringInput(taskInput['value']);
-        const HTMLString = `<li> ${task} <button class="delete-button">Delete Task</button> </li>`;
+        const taskValue = sanitizeStringInput(taskInput['value'].toString());
+        const HTMLString = `<li> ${taskValue} <span class="delete-button-for-task"><button class="delete-button">Delete Task</button> </span></li>`;
 
-        if (timeOfDay === "morning") {
-            const morningTask = document.querySelector("#morning-task-list ol");
-            morningTask.innerHTML += HTMLString;
-
-            /*
-            The for loop traverse through the NodeList of delete buttons (e.g., sample array would be [button.delete-button, button.delete-button, button.delete-button])
-            and adds an event listener to each button. The event listener is a function that removes the parent element (i.e., the list item) of the button that was clicked.
-             */
-            for (const button of document.querySelectorAll('.delete-button')) {
-                button.addEventListener('click', function() {
-                    button.parentElement.remove();
-                });
-            }
-        }
-        if (timeOfDay === "afternoon") {
-            const afternoonTask = document.querySelector("#afternoon-task-list ol");
-            afternoonTask.innerHTML += HTMLString;
-        }
-        if (timeOfDay === "evening") {
-            const eveningTask = document.getElementById("#evening-task-list ol");
-            eveningTask.innerHTML += HTMLString;
-        }
+        const timeSpecificTask = document.querySelector(`#${timeOfDay}-task-list`);
+        timeSpecificTask.innerHTML += HTMLString;
+        deleteTask(document.querySelectorAll('.delete-button-for-task'));
     }
+}
 
+function deleteTask(querySelector) {
+    /*
+    The for loop traverse through the NodeList of delete buttons
+    (e.g., sample array would be delete-button-for-task: NodeList(3) [span.delete-button-for-task, span.delete-button-for-task, span.delete-button-for-task]
+    and adds an event listener to each button. The event listener is a function that removes the parent element (i.e., the list item) of the button that was clicked.
+     */
+
+    for (const button of querySelector) {
+        button.addEventListener('click', function() {
+            button.parentElement.remove();
+        });
+
+    }
 }
 
 /*
